a7f5ced0a036aceeb415b03a9d2d49e7926b05d0
k20016755 <92389367+k20016755@users.noreply.github.com>
2023-03-25 21:22:22 +0000

    Update test_models.py

---
+++ mydjangoproject/mydjangoapp/tests/test_models.py
@@ -0,0 +1,8 @@
+import pytest
+from mydjangoapp.models import Task
+
+@pytest.mark.django_db
+def test_create_task():
+    task = Task.objects.create(title='Test Task', description='Test description')
+    assert task.title == 'Test Task'
+    assert task.description == 'Test description'

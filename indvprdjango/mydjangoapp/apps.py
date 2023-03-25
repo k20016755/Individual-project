abefec8fc3d7961d772cda951e06f3027a47bb5d
k20016755 <rafay.ahmed@kcl.ac.uk>
2023-03-25 19:16:19 +0000

    still trying

--- indvprdjango/mydjangoapp/apps.py
+++ indvprdjango/mydjangoapp/apps.py
@@ -5,4 +5,4 @@ class MydjangoappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mydjangoapp'
     def ready(self):
-        from models import Task
+        from mydjangoapp.models import Task

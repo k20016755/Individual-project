38b9ecd9aad9d0f568d945ccde7a662535a04403
k20016755 <rafay.ahmed@kcl.ac.uk>
2023-03-25 16:45:53 +0000

    updating apps.py

--- indvprdjango/mydjangoapp/apps.py
+++ indvprdjango/mydjangoapp/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class MydjangoappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mydjangoapp'
+    def ready(self):
+        from mydjangoapp.models import Task

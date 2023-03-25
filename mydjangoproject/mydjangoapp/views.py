f5032683e67a2d5a887ee7b824292108ea112073
k20016755 <92389367+k20016755@users.noreply.github.com>
2023-03-25 21:08:38 +0000

    Update views.py

--- mydjangoproject/mydjangoapp/views.py
+++ mydjangoproject/mydjangoapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Task
 
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'myapp/task_list.html', {'tasks': tasks})
 # Create your views here.

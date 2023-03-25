085b113fec77abe63598e0808b7ad40626923f89
k20016755 <92389367+k20016755@users.noreply.github.com>
2023-03-25 21:11:27 +0000

    Update urls.py

--- mydjangoproject/mydjangoproject/urls.py
+++ mydjangoproject/mydjangoproject/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import task_list
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('tasks/', task_list, name='task_list'),
 ]

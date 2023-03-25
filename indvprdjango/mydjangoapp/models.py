5c3f39f0c705b1cb683b759cfe821dded46a62c2
k20016755 <rafay.ahmed@kcl.ac.uk>
2023-03-25 20:56:20 +0000

    changing django project

--- indvprdjango/mydjangoapp/models.py
+++
@@ -1,7 +0,0 @@
-from operator import mod
-from django.db import models
-class Task(models.Model):
-    class Meta():
-        app_label='mydjangoapp'
-    title = models.CharField(max_length=255)
-    completed = models.BooleanField(default=False)
--- mydjangoproject/mydjangoapp/apps.py
+++ mydjangoproject/mydjangoapp/apps.py
@@ -4,5 +4,3 @@ from django.apps import AppConfig
 class MydjangoappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mydjangoapp'
-    def ready(self):
-        from mydjangoapp.models import Task

a92313604cc8cb00ad768dc91de7ef5fbf1686dd
k20016755 <rafay.ahmed@kcl.ac.uk>
2023-03-27 02:32:30 +0100

    removing django files from main

--- mydjangoproject/mydjangoapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Task
-
-def task_list(request):
-    tasks = Task.objects.all()
-    return render(request, 'mydjangoapp/templates/task_list.html', {'tasks': tasks})
-# Create your views here.

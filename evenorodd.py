bc5671b42deb4f68a3b49bfa22078fa7c9db1c6d
k20016755 <rafay.ahmed@kcl.ac.uk>
2023-04-12 02:58:25 +0100

    fixing unittests

--- evenorodd.py
+++ evenorodd.py
@@ -1,38 +1,11 @@
-#example.py
-
-
 
 import unittest
 import random
 
-class BankAccount:
-  def __init__(self, id):
-    self.bankid = id
-    self.balance = 0
-
-  def withdraw(self, amount):
-    if self.balance >= amount:
-      self.balance -= amount
-      return True
-    return False
-
-  def deposit(self, amount):
-    self.balance += amount
-    return True
-
-
-   
-class TestBankOperations(unittest.TestCase):
-    def test_low_deposit(self):
-      a = BankAccount(1)
-      a.deposit(100)
-      outcome = a.withdraw(200)
-      self.assertFalse(outcome)
 class evenorodd:
     def check(num):
         if (num%2)==0:
             print ("Number is even")
-
         else:
             print("Number is odd")
 
